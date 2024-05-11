e9c7b77164b1ef0d3fa89c0c67d463ca8241a929
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-05-11 16:46:20 +0300

    Add a minimalistic bear example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+
 import Formulas from "@/components/Formulas";
 import ZustandCounter from "@/components/ZustandCounter";
+
+import { create } from "zustand";
+
+type BearState = {
+  bears: number;
+  increasePopulation: () => void;
+  removeAllBears: () => void;
+};
+
+const useBearStore = create<BearState>((set) => ({
+  bears: 0,
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+}));
+
+function BearCounter() {
+  const bears = useBearStore((state) => state.bears);
+  return <h1>{bears} around here ...</h1>;
+}
+
+function Controls() {
+  const increasePopulation = useBearStore((state) => state.increasePopulation);
+  return <button onClick={increasePopulation}>one up</button>;
+}
 
 export default function Home() {
   return (
@@ -7,6 +33,11 @@ export default function Home() {
       <h1 className="text-2xl font-bold mb-4">Homepage</h1>
       <Formulas />
       <ZustandCounter></ZustandCounter>
+
+      <div>
+        <BearCounter />
+        <Controls />
+      </div>
     </div>
   );
 }
